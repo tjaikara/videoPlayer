4ad6b36886468d8add4f8c048dd2d4aedadb27c3
tjaikara <tonyaikara@gmail.com>
2017-06-04 20:26:18 -0500

    Update player.js

--- player.js
+++ player.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function(){
 
 	//Video Container
 	video= document.getElementById('video');
+	pauseScreen = document.getElementById('screen');
+	screenButton = document.getElementById('screen-button');
 
 	//Progress bar container
 	pbarContainer = document.getElementById('pbar-container');
@@ -10,12 +12,21 @@ window.addEventListener('load', function(){
 	//Buttons container
 	playButton = document.getElementById('play-button');
 	timeField = document.getElementById('time-field');
+	soundBotton = document.getElementById('sound-button');
+	sbarContainer = document.getElementById('sbar-container');
+	sbar = document.getElementById('sbar');
+	fullscreenButtom = document.getElementById('fullscreen-button';)
 
 
 	video.load();
 	video.addEventListener('canplay', function(){
 		playButton.addEventListener('click', playorPause, false);
 		pbarContainer.addEventListener('click', skip, false);
+		updtePlayer();
+		soundBotton.addEventListener('click', muteOrUnmute, false);
+		sbarContainer.addEventListener('click', updateVolume, false);
+		fullscreenButtom.addEventListener('click', fullScreen, false);
+		screenButton.addEventListener('click', playorPause, false);
 	}, false)
 	
 }, false);
@@ -26,11 +37,17 @@ function playorPause(){
 		playButton.src = 'img/pause.png';
 		update = setInterval(updtePlayer, 30);
 
+		pauseScreen.style.display = 'none';
+		screenButton.src = 'img/play.png';
+
 	}
 	else{
 		video.pause();
 		playButton.src = 'img/play.png';
 		window.clearInterval(update);
+
+		pauseScreen.style.display = 'block';
+		screenButton.src = 'img/play.png';
 	}
 }
 
@@ -41,6 +58,13 @@ function updtePlayer(){
 	if(video.ended){
 		window.clearInterval(update);
 		playButton.src = 'img/replay.png';
+
+		pauseScreen.style.display = 'block';
+		screenButton.src = 'img/replay.png';
+	}
+	else if (video.paused){
+		playButton.src = 'img/play.png';
+		screenButton.src = 'img/play.png';
 	}
 }
 
@@ -61,9 +85,56 @@ function getFormatedTime(){
 	if(minutes > 0) seconds -= minutes*60;
 	if(seconds.toString().length === 1) seconds= '0'+seconds;
 
-	if(minutes.toString().length === 1) minutes= '0' + minutes;
-
-	var totalSeconds = Math.round(video.duaration);
+	var totalSeconds = Math.round(video.duration);
 	var totalMinutes = Math.floor(totalSeconds/60);
-	return minutes+ ':' + seconds;
+	if(totalMinutes > 0) totalSeconds -=totalMinutes*60;
+	if(totalSeconds.toString().length === 1) totalSeconds = '0'+ totalSeconds;
+
+	return minutes+ ':' + seconds + '/' + totalMinutes + ':' + totalSeconds;
+}
+
+function muteOrUnmute(){
+	if(!video.muted){
+		video.muted = true;
+		soundBotton.src = 'img/mute.png'
+		sbar.style.display = 'none';
+
+	}
+	else{
+		video.muted = false;
+		soundBotton.src = 'img/sound.png';
+		sbar.style.display = 'block';
+	}
+}
+
+function updateVolume(ev){
+
+	var mouseX = ev.pageX - sbarContainer.offsetLeft;
+	var barWidth = window.getComputedStyle(sbarContainer).getPropertyValue('width');
+	barWidth = parseFloat(barWidth.substr(0, barWidth.length - 2));
+
+	video.volume = (mouseX/barWidth);
+	sbar.style.width = (mouseX/barWidth)*100 + '%';
+	video.muted = false;
+	soundBotton.src = 'img/sound.png';
+	sbar.style.display = 'block';
+
+
+}
+
+function fullScreen(){
+
+	if(video.requestFullscreen){
+		video.requestFullscreen();
+	}
+	else if (video.webkitRequestFullscreen){
+		video.webkitRequestFullscreen();
+	}
+	else if(video.mozRequestFullscreen){
+		video.mozRequestFullscreen();
+	}
+	else if(video.mozRequestFullscreen){
+		video.mozRequestFullscreen();
+	}
+
 }
